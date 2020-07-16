87643e135b42a1eb764afee183ae39c5462a5b64
plgitdev <plyssikatos@gmail.com>
2020-07-16 17:28:16 -0600

    I think it should go like this two seperate ifs

--- src/main/java/com/example/movingaverage/Main.java
+++ src/main/java/com/example/movingaverage/Main.java
@@ -65,19 +65,20 @@ public class Main {
 
                             }
                             //reset the historical data
-                            if (LocalDateTime.now().equals(priceObj.getTimestamp().plusDays(inputL2))) {
+                            if (LocalDateTime.now().equals(priceObj.getTimestamp().plusDays(inputL))) {
                                 priceObj.getPriceShorter().clear();
-                                priceObj.getPriceLonger().clear();
                                 mongoCRUD
                                     .retrieveMarketDataByDays("marketSummary",
                                         inputL, "Timestamp","Last").forEach( (data) -> priceObj
                                             .addPriceShorter((Double) data.get("Last")));
+                            if (LocalDateTime.now().equals(priceObj.getTimestamp().plusDays(inputL2)))
+                                priceObj.getPriceLonger().clear();
                                 mongoCRUD
                                     .retrieveMarketDataByDays("marketSummary",
                                         inputL2, "Timestamp","Last").forEach( (data) -> priceObj
                                             .addPriceLonger((Double)(data.get("Last"))));
                                 priceObj.setTimestamp(LocalDateTime.now());
-                            }
+
                             priceObj.dateLimitCheck();
                         }
                     } catch (Exception e) {
