7c21871340e88bfb3a155c508beb645d272f90a3
plgitdev <plyssikatos@gmail.com>
2021-03-01 19:02:53 -0800

    encryption interface

--- src/main/java/Live/Encryption.java
+++ src/main/java/Live/Encryption.java
@@ -5,5 +5,8 @@ import java.security.NoSuchAlgorithmException;
 public interface Encryption {
      String HMAC_SHA512 = "HmacSHA512";
      String SHA512 = "SHA512";
-     String create512Hash(Object content) throws NoSuchAlgorithmException;
+     String createHash(Object content)  throws NoSuchAlgorithmException;
+     String createSecureHash(Object content);
+     StringBuilder zeroPad(StringBuilder s, int totalBits);
+
 }
--- src/main/java/Live/Transaction.java
+++ src/main/java/Live/Transaction.java
@@ -1,11 +1,8 @@
 package Live;
 import com.example.movingaverage.Global;
-import jdk.nashorn.internal.runtime.regexp.joni.ast.StringNode;
-
 import java.io.IOException;
 import java.math.BigInteger;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -19,7 +16,7 @@ public abstract class Transaction implements Encryption {
     protected Double limit;
     protected String timeInForce;
     protected String direction;
-    protected Map<Object,Object> content;
+    protected Map<Object, Object> content;
     protected String contentH;
     protected LocalDateTime timestamp;
     protected URL uri;
@@ -27,24 +24,29 @@ public abstract class Transaction implements Encryption {
     public abstract int send() throws IOException, NoSuchAlgorithmException;
 
     public final void setHeaders(HttpURLConnection http) {
-        http.setRequestProperty("Api-Key","API-KEY");
+        http.setRequestProperty("Api-Key", "API-KEY");
         http.setRequestProperty("Api-Timestamp", timestamp.toString());
-        http.setRequestProperty("Api-Content-Hash",contentH);
-        http.setRequestProperty("Api-Signature","API-SIGNATURE");
+        http.setRequestProperty("Api-Content-Hash", contentH);
+        http.setRequestProperty("Api-Signature", "API-SIGNATURE");
     }
 
     public final void setContentHash() throws NoSuchAlgorithmException {
-        this.contentH = create512Hash(this.content);
+        this.contentH = createHash(this.content);
     }
-    public final String create512Hash(Object content) throws NoSuchAlgorithmException {
+
+    public final String createHash(Object content) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance(SHA512);
         byte[] messageDigest = md.digest(content.toString().getBytes());
         BigInteger signumRep = new BigInteger(1, messageDigest);
         StringBuilder hash = new StringBuilder(signumRep.toString(16));
-        while(hash.length() < 32)  {
+        return zeroPad(hash, 32).toString();
+    }
+
+    public final StringBuilder zeroPad(StringBuilder hash, int totalBits) {
+        while (hash.length() < 32) {
             hash.insert(0, "0");
         }
-        return hash.toString();
+        return hash;
     }
 }
 
