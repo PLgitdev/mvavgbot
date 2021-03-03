02482f936b50abdbe35455924e2543e56b1f0307
plgitdev <plyssikatos@gmail.com>
2021-03-03 08:30:38 -0800

    lets try it

--- src/main/java/com/example/movingaverage/Keys.java
+++ src/main/java/com/example/movingaverage/Keys.java
@@ -1,6 +1,6 @@
 package com.example.movingaverage;
 
 public class Keys {
-    final public static String API_KEY  = "api";
-    final public static  String SECRET_API_KEY  = "secretApi";
+    final public static String API_KEY  = "3654876db9bd4ab289564493bed7e5a0";
+    final public static  String SECRET_API_KEY  = "8f16bcd61b3f4529b5e3bb3eb91fbeda";
 }
