749a38cfa2d3732d661351064ba73ca79886d78b
plgitdev <plyssikatos@gmail.com>
2021-04-01 12:47:18 -0600

    wip

--- src/test/java/com/example/movingaverage/Model/PriceTest.java
+++ src/test/java/com/example/movingaverage/Model/PriceTest.java
@@ -1,9 +1,7 @@
 package com.example.movingaverage.Model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
