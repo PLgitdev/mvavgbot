01ece53b3d3f219c82846893feaeaf403c9e9f2c
PLgitdev <50424549+PLgitdev@users.noreply.github.com>
2021-04-03 08:00:30 -0600

    OOOOPS
    omfg

--- src/main/java/com/example/movingaverage/Keys.java
+++
@@ -1,6 +0,0 @@
-package com.example.movingaverage;
-
-public class Keys {
-    final public static String API_KEY  = "d0e8575c1e094525aa4c45fe95b3c3d2";
-    final public static  String SECRET_API_KEY  = "998f70e17c2a44fc80f11a86a69cb23b";
-}
